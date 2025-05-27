92215aac4d8a2135dcb0bbef6def3623a9972b09
Ismael <ismael.henrique.dev@gmail.com>
2025-05-27 10:17:58 -0300

    feat(profile): implement user profile fetching and display in UserInfoCard

--- src/components/profile/user-info-card/index.tsx
+++ src/components/profile/user-info-card/index.tsx
@@ -1,16 +1,35 @@
 import { Text, View } from 'react-native'
 import { styles } from './styles'
+import { useEffect, useState } from 'react'
+import { getProfile, GetProfileResponse } from '@/services/http/user/get-profile'
+import { getInitials } from '@/utils/get-initials'
+
+type UserData = Pick<GetProfileResponse, 'data'>
 
 export function UserInfoCard() {
+  const [userData, setUserData] = useState<UserData | null>(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getProfile()
+      console.log('Ranking data', response)
+
+      if (response) {
+        setUserData(response)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <View style={styles.userInfoContainer}>
       <View style={styles.userInfoAvatar}>
-        <Text style={styles.userInfoAvatarText}>IH</Text>
+        <Text style={styles.userInfoAvatarText}>{getInitials(userData?.data.username!)}</Text>
       </View>
       <View style={styles.userInfoTextContainer}>
-        <Text style={styles.userInfoUsernameText}>Ismael Henrique</Text>
+        <Text style={styles.userInfoUsernameText}>{userData?.data.username!}</Text>
         <Text style={styles.userInfoUserEmailText}>
-          ismaelhenrique@gmail.com
+          {userData?.data.email!}
         </Text>
       </View>
     </View>
---
+++ src/services/http/user/get-profile.ts
@@ -0,0 +1,38 @@
+import { api } from '@/services/api'
+import { getAxiosStatusCode } from '@/utils/get-axios-status-code'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export type GetProfileResponse = {
+  data: { username: string; email: string; imageUrl: string }
+  Description: string
+}
+
+export async function getProfile(): Promise<GetProfileResponse | undefined> {
+  try {
+    const token = await AsyncStorage.getItem('@token')
+    console.log(token)
+
+    if (token) {
+      const response = await api.get<GetProfileResponse>('user/get', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      return response.data
+    }
+  } catch (error) {
+    const statusCode = getAxiosStatusCode(error)
+
+    switch (statusCode) {
+      case 400:
+        throw new Error('Senha inválida.')
+      case 404:
+        throw new Error('Usuário não encontrado.')
+      case 500:
+        throw new Error('Erro interno no servidor.')
+      default:
+        throw new Error('Erro desconhecido.')
+    }
+  }
+}
